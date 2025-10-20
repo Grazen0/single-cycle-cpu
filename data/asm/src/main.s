070df8ad98cfd4836bf7de0eab8f2a7aeb791214
José Daniel Grayson <josedanielgrayson@proton.me>
2025-10-19 23:24:04 -0500

    refactor: optimize assembly

--- data/asm/src/main.s
+++ data/asm/src/main.s
@@ -10,92 +10,88 @@ LCD_RETURN = 0b00000010
 LCD_SET = 0b00000111
 
 _start:
-    li      s1, LCD_DATA
-    li      s2, LCD_OPTS
-    li      s3, LCD_ENABLE
-    li      s4, 1
+    sw      zero, LCD_OPTS(zero) # write instruction
 
-    li      s0, 0b00 # write instruction
-    sw      s0, 0(s2)
+    li      s1, 1
 
     li      s0, LCD_CLEAR
-    sw      s0, 0(s1)
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s0, LCD_DATA(zero)
+    sw      s1, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
     li      s0, LCD_RETURN
-    sw      s0, 0(s1)
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s0, LCD_DATA(zero)
+    sw      s4, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
     li      s0, LCD_SET
-    sw      s0, 0(s1)
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s0, LCD_DATA(zero)
+    sw      s1, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
     li      s0, 0b10 # write data
-    sw      s0, 0(s2)
+    sw      s0, LCD_OPTS(zero)
 
     li      s0, 'H'
-    sw      s0, 0(s1)
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s0, LCD_DATA(zero)
+    sw      s1, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
     li      s0, 'e'
-    sw      s0, 0(s1)
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s0, LCD_DATA(zero)
+    sw      s1, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
     li      s0, 'l'
-    sw      s0, 0(s1)
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s0, LCD_DATA(zero)
+    sw      s1, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s1, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
     li      s0, 'o'
-    sw      s0, 0(s1)
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s0, LCD_DATA(zero)
+    sw      s1, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
     li      s0, ','
-    sw      s0, 0(s1)
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s0, LCD_DATA(zero)
+    sw      s1, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
     li      s0, ' '
-    sw      s0, 0(s1)
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s0, LCD_DATA(zero)
+    sw      s4, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
     li      s0, 'w'
-    sw      s0, 0(s1)
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s0, LCD_DATA(zero)
+    sw      s4, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
     li      s0, 'o'
-    sw      s0, 0(s1)
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s0, LCD_DATA(zero)
+    sw      s4, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
     li      s0, 'r'
-    sw      s0, 0(s1)
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s0, LCD_DATA(zero)
+    sw      s4, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
     li      s0, 'l'
-    sw      s0, 0(s1)
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s0, LCD_DATA(zero)
+    sw      s4, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
     li      s0, 'd'
-    sw      s0, 0(s1)
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s0, LCD_DATA(zero)
+    sw      s4, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
     li      s0, '!'
-    sw      s0, 0(s1)
-    sw      s4, 0(s3)
-    sw      zero, 0(s3)
+    sw      s0, LCD_DATA(zero)
+    sw      s4, LCD_ENABLE(zero)
+    sw      zero, LCD_ENABLE(zero)
 
